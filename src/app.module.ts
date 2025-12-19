ad22cfd5d8f3831fb79e7abc5272070f3537b590
Carlos Huguez <carlos.huguez@sonorasoft.com>
2025-12-19 11:58:31 -0700

    docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports:     [],
-  controllers: [],
-  providers:   [],
-  exports:     []
+   imports: [
+      ConfigModule.forRoot(),
+      TypeOrmModule.forRoot({
+         type: 'postgres',
+         host: 'localhost',
+         port: 5432,
+         database: 'Teslodb',
+         username: 'postgres',
+         password: 'admin123',
+         autoLoadEntities: true,
+         synchronize: true,
+      })
+   ],
+   controllers: [],
+   providers: [],
+   exports: []
 })
-export class AppModule {}
+export class AppModule { }
+console.log( process.env.DB_USER );
